00d0fd95c06b0ff00a3d04bdc02d3960d876c6cc
Joël Lauper <jlauper@ethz.ch>
2026-06-09 15:55:58 +0200

    resolved conflicts

--- zen_garden/postprocess/comparisons.py
+++ zen_garden/postprocess/comparisons.py
@@ -135,11 +135,7 @@ def get_component_diff(
             for name, component in results_0.solution_loader.scenarios[
                 scenarios[0]
             ].components.items()
-<<<<<<< HEAD
-            if component["component_type"] is component_type
-=======
             if component["component_type"] is component_type and "_units" not in name
->>>>>>> main
         ]
     )
 
@@ -150,11 +146,7 @@ def get_component_diff(
             for name, component in results_1.solution_loader.scenarios[
                 scenarios[1]
             ].components.items()
-<<<<<<< HEAD
-            if component["component_type"] is component_type
-=======
             if component["component_type"] is component_type and "_units" not in name
->>>>>>> main
         ]
     )
     only_in_0 = component_names_0.difference(component_names_0)
